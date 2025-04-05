668603ed401f0cbe9fe7f3c27aff5585cc338d8d
Ryan-Nelson1 <ranelso@clemson.edu>
2025-04-05 10:58:10 -0400

    Finished downloader.py

--- downloader.py
+++ downloader.py
@@ -4,6 +4,22 @@ suspiciousIPUrl = "https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.
 filename = "suspicious_IP.txt"
 urllib.request.urlretrieve(suspiciousIPUrl,filename)
 
-f = open(filename, 'r')
+f_in = open(filename, 'r')
 
-print(f.read())
+# print(f.read())
+
+maliciousIPs = []
+
+for line in f_in:
+    if line[0] != '\n' and line[0] != '#':
+        maliciousIPs.append(line.strip())
+        # print(line)
+
+# print(filename[:-4] + '_Prepared.txt')
+f_out = open(filename[:-4] + '_Prepared.txt','w')
+
+for IP in maliciousIPs:
+    f_out.write(IP + "\n")
+
+f_out.close()
+f_in.close()
